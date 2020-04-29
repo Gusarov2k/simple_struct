51313a13f2539dd2d8abed8c4046cd95674f83b0
ivanzabelin <motoface@yandex.ru>
2020-04-29 10:48:42 +0300

    start 3

--- internal/figure/base_test.go
+++ internal/figure/base_test.go
@@ -1,43 +1,43 @@
 package figure_test
 
 import (
-	"github.com/Gusarov2k/simple_struct/internal/figure"
+	f "github.com/Gusarov2k/simple_struct/internal/figure"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestPassSquareArea(t *testing.T) {
-	square := figure.Square{}
+	square := f.Square{}
 	result := square.Area(2)
 	assert.Equal(t, result, float64(4))
 }
 
 func TestPassSquarePerimeter(t *testing.T) {
-	square := figure.Square{}
+	square := f.Square{}
 	result := square.Perimeter(2)
 	assert.Equal(t, result, float64(8))
 }
 
 func TestPassTrianglePerimeter(t *testing.T) {
-	triangle := figure.Triangle{}
+	triangle := f.Triangle{}
 	result := triangle.Perimeter(2, 3, 5)
 	assert.Equal(t, result, float64(10))
 }
 
 func TestPassTriangleArea(t *testing.T) {
-	triangle := figure.Triangle{}
+	triangle := f.Triangle{}
 	result := triangle.Area(2, 2, 2)
 	assert.Equal(t, result, float64(1.7320508075688772))
 }
 
 func TestPassRectangleArea(t *testing.T) {
-	rectangle := figure.Rectangle{}
+	rectangle := f.Rectangle{}
 	result := rectangle.Area(2)
 	assert.Equal(t, result, float64(12.566370614359172))
 }
 
 func TestPassRectanglePerimeter(t *testing.T) {
-	rectangle := figure.Rectangle{}
+	rectangle := f.Rectangle{}
 	result := rectangle.Perimeter(2)
 	assert.Equal(t, result, float64(12.566370614359172))
 }
